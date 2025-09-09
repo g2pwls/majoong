2b43408f17b3f6720c7c4d9eb895289cc238e965
swgm0719 <swgm0719@naver.com>
2025-09-09 10:17:20 +0900

    chore: backend 폴더 재생성

---
+++ backend/src/test/java/com/e105/machimnae/MachimnaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e105.machimnae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachimnaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
