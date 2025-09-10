29902bb87cce6139d4d23dd159f2a879bdfc9cdd
iqveou6 <swgm0719@naver.com>
2025-09-11 01:18:54 +0900

    test: backend jenkins cicd test

--- backend/src/main/java/com/e105/machimnae/common/utils/CodeGenerator.java
+++ backend/src/main/java/com/e105/machimnae/common/utils/CodeGenerator.java
@@ -2,7 +2,7 @@ package com.e105.machimnae.common.utils;
 
 import java.util.UUID;
 
-public class CodeGenerator {  
+public class CodeGenerator {   
 
     public static String generateCode(int length) {    
         return UUID.randomUUID().toString().substring(0, length).toUpperCase();
