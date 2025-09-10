a9e008286ff78d04cdfbba2358f3b8eb7a937fca
iqveou6 <swgm0719@naver.com>
2025-09-11 00:38:20 +0900

    refactor: Detect Branch 수정

--- backend/src/main/java/com/e105/machimnae/common/utils/CodeGenerator.java
+++ backend/src/main/java/com/e105/machimnae/common/utils/CodeGenerator.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public class CodeGenerator {
 
-    public static String generateCode(int length) {  
+    public static String generateCode(int length) {   
         return UUID.randomUUID().toString().substring(0, length).toUpperCase();
     }
 
