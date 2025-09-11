d732daaf17c58518a0067c823ce21807e860ce66
swgm0719 <swgm0719@naver.com>
2025-09-11 15:26:44 +0900

    chore: frontend dockerfile 수정 및 dockerignore 생성

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // next.config.ts
+  output: "standalone",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
