15f4c992ab2f1c599d24ec4bcc6eb74731a5c70f
swgm0719 <swgm0719@naver.com>
2025-09-11 09:22:15 +0900

    test: test 용 commit

--- backend/src/main/java/com/e105/machimnae/common/utils/CodeGenerator.java
+++ backend/src/main/java/com/e105/machimnae/common/utils/CodeGenerator.java
@@ -2,10 +2,10 @@ package com.e105.machimnae.common.utils;
 
 import java.util.UUID;
 
-public class CodeGenerator {  
+public class CodeGenerator {
   
     public static String generateCode(int length) {    
         return UUID.randomUUID().toString().substring(0, length).toUpperCase();
     }
-
+   
 }
