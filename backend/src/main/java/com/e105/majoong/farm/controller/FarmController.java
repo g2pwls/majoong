39251d92d8de3c651519257ef63afed0d45df3f1
heesan <heesan6615@naver.com>
2025-09-19 00:30:36 +0900

    fix: Operation summary 추가 및 시큐리티 수정

--- backend/src/main/java/com/e105/majoong/farm/controller/FarmController.java
+++ backend/src/main/java/com/e105/majoong/farm/controller/FarmController.java
@@ -7,6 +7,7 @@ import com.e105.majoong.farm.service.*;
 import com.e105.majoong.report.dto.out.MonthlyReportDetailResponseDto;
 import com.e105.majoong.report.dto.out.MonthlyReportListResponseDto;
 import com.e105.majoong.report.service.MonthlyReportService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,7 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/farms")
 @RequiredArgsConstructor
-@Tag(name = "Farm API", description = "농장 관련 API")
+@Tag(name = "Farm API", description = "목장 조회 관련 API")
 public class FarmController {
 
     private final FarmService farmService;
@@ -29,6 +30,7 @@ public class FarmController {
     private final ScoreService scoreService;
 
     @GetMapping
+    @Operation(summary = "목장 키워드로 농장 목록 조회")
     public BaseResponse<Page<FarmListResponseDto>> searchFarms(
             @RequestParam(required = false) String farmName,
             @RequestParam(defaultValue = "0") int page,
@@ -42,6 +44,7 @@ public class FarmController {
     }
 
     @GetMapping("/{farmUuid}")
+    @Operation(summary = "목장 상세의 목장 소개 조회")
     public BaseResponse<FarmDetailResponseDto> getFarmDetail(
             @PathVariable String farmUuid
     ) {
@@ -49,6 +52,7 @@ public class FarmController {
     }
 
     @GetMapping("/{farmUuid}/monthly-reports/{reportId}")
+    @Operation(summary = "목장 상세의 월간 보고서 목록 조회")
     public BaseResponse<MonthlyReportDetailResponseDto> getReportDetail(
             @PathVariable String farmUuid,
             @PathVariable Long reportId
@@ -57,6 +61,7 @@ public class FarmController {
     }
 
     @GetMapping("/{farmUuid}/monthly-reports")
+    @Operation(summary = "목장 상세의 월간 보고서 상세 조회")
     public BaseResponse<List<MonthlyReportListResponseDto>> getReports(
             @PathVariable String farmUuid,
             @RequestParam int year
@@ -65,6 +70,7 @@ public class FarmController {
     }
 
     @GetMapping("/{farmUuid}/donations/usage")
+    @Operation(summary = "목장 상세의 기부금 사용 내역 목록 조회")
     public DonationUsageResponseDto getDonationUsage(
             @PathVariable String farmUuid,
             @RequestParam int year,
@@ -74,6 +80,7 @@ public class FarmController {
     }
 
     @GetMapping("/horses")
+    @Operation(summary = "말 키워드로 말 조회")
     public BaseResponse<Page<HorseSearchResponseDto>> searchHorses(
             @RequestParam(required = false) String horseName,
             @RequestParam(defaultValue = "0") int page,
@@ -84,12 +91,14 @@ public class FarmController {
     }
 
     @GetMapping("/{farmUuid}/donations/usage/last-month")
+    @Operation(summary = "목장 상세 기부금 사용 내역의 지난 달 기부금 사용 내역 조회(통계)")
     public BaseResponse<LastMonthUsageResponseDto> getLastMonthUsage(
             @PathVariable String farmUuid) {
         return new BaseResponse<>(monthlyDonationService.getLastMonthUsage(farmUuid));
     }
 
     @GetMapping("/{farmUuid}/donations/usage/{usageId}")
+    @Operation(summary = "기부금 사용 내역 상세 조회")
     public BaseResponse<UsageDetailResponseDto> getUsageDetail(
             @PathVariable String farmUuid,
             @PathVariable Long usageId
@@ -99,11 +108,13 @@ public class FarmController {
     }
 
     @GetMapping("/{farmUuid}/scores")
+    @Operation(summary = "목장 상세의 신뢰도 내역 (통계)")
     public BaseResponse<List<ScoreHistoryAvgResponseDto>> getScoreHistory(@PathVariable String farmUuid, @RequestParam int year) {
         return new BaseResponse<>(scoreService.getScoreHistory(farmUuid, year));
     }
 
     @GetMapping("/{farmUuid}/horses/{horseNumber}")
+    @Operation(summary = "말 상세 정보 및 주간 보고서 목록 조회")
     public BaseResponse<HorseDetailResponseDto> getHorseDetail(
             @PathVariable String farmUuid,
             @PathVariable Long horseNumber,
@@ -114,6 +125,7 @@ public class FarmController {
     }
 
     @GetMapping("/{farmUuid}/scores/history")
+    @Operation(summary = "목장 상세의 신뢰도 내역 목록 조회")
     public BaseResponse<List<ScoreHistoryResponseDto>> getScoreHistory(@PathVariable String farmUuid) {
         List<ScoreHistoryResponseDto> result = scoreService.getScoreHistory(farmUuid);
         return new BaseResponse<>(result);
