8df024315c67304451e2bad2c5d522eded69ad1c
heesan <heesan6615@naver.com>
2025-09-18 14:44:42 +0900

    !HOTFIX: 오타 수정

--- backend/src/main/java/com/e105/majoong/common/model/horseState/HorseStateRepository.java
+++ backend/src/main/java/com/e105/majoong/common/model/horseState/HorseStateRepository.java
@@ -1,6 +1,5 @@
 package com.e105.majoong.common.model.horseState;
 
-<<<<<<< backend/src/main/java/com/e105/majoong/common/model/horseState/HorseStateRepository.java
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
