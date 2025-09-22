a1c19f92962b508dd70a0845d052fea5335159a4
swgm0719 <swgm0719@naver.com>
2025-09-22 12:26:24 +0900

    fix: 백엔드 yml 파일 업데이트

--- backend/src/main/java/com/e105/majoong/MajoongApplication.java
+++ backend/src/main/java/com/e105/majoong/MajoongApplication.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaAuditing
 @EnableBatchProcessing
 @SpringBootApplication
-@EnableJpaAuditing
 public class MajoongApplication {
      
 	public static void main(String[] args) {
