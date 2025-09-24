d049c67b57c028d667fff475fb3f0e45247773ce
swgm0719 <swgm0719@naver.com>
2025-09-24 14:35:50 +0900

    feat: 농장 수정 api  구현

--- backend/src/main/java/com/e105/majoong/mypage/service/FarmerMyPageService.java
+++ backend/src/main/java/com/e105/majoong/mypage/service/FarmerMyPageService.java
@@ -1,10 +1,12 @@
 package com.e105.majoong.mypage.service;
 
-import com.e105.majoong.mypage.dto.out.DonationResponseDto;
 import com.e105.majoong.mypage.dto.out.VaultResponseDto;
 import java.time.LocalDate;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface FarmerMyPageService {
     VaultResponseDto getVaultHistoryByPage(
             String memberUuid, int page, int size, LocalDate startDate, LocalDate endDate);
+
+    void updateFarmers(String memberUuid, String farmName, String phoneNumber, MultipartFile image);
 }
