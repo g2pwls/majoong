7f51061553a62e043e6551c9449fe36b847a8e40
heesan <heesan6615@naver.com>
2025-09-26 12:42:01 +0900

    refactor: 주석 제거

--- frontend/src/app/qr-login/page.tsx
+++ frontend/src/app/qr-login/page.tsx
@@ -21,17 +21,16 @@ export default function QrLoginPage() {
         const data = await res.json();
         const result = data.result;
 
-        // 기존 로그인과 똑같이 localStorage에 저장
+
         localStorage.setItem("accessToken", result.accessToken);
         localStorage.setItem("refreshToken", result.refreshToken);
         localStorage.setItem("email", result.email);
         localStorage.setItem("role", result.role);
 
-        // ✅ Navbar에게 로그인 상태 바뀌었다고 알려주기
+ 
         window.dispatchEvent(new Event("authStateChanged"));
 
-        // ✅ 바로 기부 페이지로 이동
-        router.replace("/godonate"); // 라우트명 확인 필요
+        router.replace("/godonate"); 
       } catch (err) {
         console.error(err);
         alert("QR 로그인 실패. 다시 시도해주세요.");
