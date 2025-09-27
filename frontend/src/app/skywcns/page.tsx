17e17a3ae33feecd5b6a151234394fff0eb4d896
parkjihyun <annn0402@gmail.com>
2025-09-27 14:07:46 +0900

    feat: skywcns 페이지 생성

---
+++ frontend/src/app/skywcns/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SkyWithCloudsAndStars from '@/components/common/SkyWithCloudsAndStars';
+
+export default function SkyWithCloudsAndStarsPage() {
+  return (
+    <div className="relative min-h-screen w-full overflow-hidden">
+      <SkyWithCloudsAndStars />
+    </div>
+  );
+}
