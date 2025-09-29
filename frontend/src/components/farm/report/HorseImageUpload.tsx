a881112ba5df94bc288688ac446b820f39007066
DESKTOP-DLOPF19\SSAFY <yhjyhw1004@naver.com>
2025-09-29 10:32:26 +0900

    fix: 모달 창 배경 수정

--- frontend/src/components/farm/report/HorseImageUpload.tsx
+++ frontend/src/components/farm/report/HorseImageUpload.tsx
@@ -605,7 +605,7 @@ export default function HorseImageUpload({
 
       {/* 성공 모달 */}
       {showSuccessModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-white/50 bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg p-6 max-w-md w-full mx-4">
             <div className="text-center">
               {/* 성공 아이콘 */}
